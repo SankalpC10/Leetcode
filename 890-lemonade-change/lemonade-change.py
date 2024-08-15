3df49505cc4c26500dc818bd70ed232a24750375
Sankalp Chavhan <55315838+SankalpC10@users.noreply.github.com>
2024-08-15 12:40:15 +0530

    Time: 625 ms (38.20%) | Memory: 21.2 MB (15.58%) - LeetSync

--- 890-lemonade-change/lemonade-change.py
+++ 890-lemonade-change/lemonade-change.py
@@ -1,24 +1,22 @@
 class Solution:
     def lemonadeChange(self, bills: List[int]) -> bool:
-        five, ten = 0, 0
-        for bill in bills:
-            if bill == 5:
-                five += 1
-            elif bill == 10:
-                ten += 1
-                five -= 1
-            else:
-                if ten > 0:
-                    ten -= 1
-                    five -= 1
+        fives = 0
+        tens = 0
+        for i in bills:
+            if i==5:
+                fives +=1
+            elif i==10:
+                if fives!=0:
+                    fives-=1
+                    tens+=1
                 else:
-                    five -= 3
-            if five < 0:
-                return "false"
-        return "true"
-
-s = Solution()
-with open('user.out', 'w') as f:
-    for case in map(loads, stdin):
-        f.write(f"{s.lemonadeChange(case)}\n")
-exit(0)
+                    return False
+            else:
+                if fives!=0 and tens!=0:
+                    fives-=1
+                    tens-=1
+                elif fives>=3:
+                    fives-=3
+                else:
+                    return False
+        return True
