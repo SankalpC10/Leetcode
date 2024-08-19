8e518e84df7326bd1980fe207060237e977490d9
Sankalp Chavhan <55315838+SankalpC10@users.noreply.github.com>
2024-08-19 22:31:35 +0530

    Time: 53 ms (13.73%) | Memory: 16.6 MB (39.98%) - LeetSync

--- 2727-number-of-senior-citizens/number-of-senior-citizens.py
+++ 2727-number-of-senior-citizens/number-of-senior-citizens.py
@@ -1,10 +1,7 @@
 class Solution:
     def countSeniors(self, details: List[str]) -> int:
-        count =0
+        c=0
         for i in details:
-            for j in range(len(i)):
-                if i[j].isalpha():
-                    age = int(i[j+1:j+3])
-                    if age > 60:
-                        count += 1
-        return count
+            if(int(i[11:13])>60):
+                c+=1
+        return c
