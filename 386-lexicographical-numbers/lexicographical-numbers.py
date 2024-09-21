98aaf0432330f589191002853299c93de280e1d3
Sankalp Chavhan <55315838+SankalpC10@users.noreply.github.com>
2024-09-21 23:52:32 +0530

    Time: 55 ms (99.40%) | Memory: 22.5 MB (74.04%) - LeetSync

--- 386-lexicographical-numbers/lexicographical-numbers.py
+++ 386-lexicographical-numbers/lexicographical-numbers.py
@@ -1,7 +1,20 @@
 class Solution:
     def lexicalOrder(self, n: int) -> List[int]:
-        res = [str(i) for i in range(1,n+1)]
-        res = sorted(res)
-        res = [int(i) for i in res]
-        return res
+        lexicographical_numbers = []
+        current_number = 1
+
+        # Generate numbers from 1 to n
+        for _ in range(n):
+            lexicographical_numbers.append(current_number)
+
+            # If multiplying the current number by 10 is within the limit, do it
+            if current_number * 10 <= n:
+                current_number *= 10
+            else:
+                # Adjust the current number by moving up one digit
+                while current_number % 10 == 9 or current_number >= n:
+                    current_number //= 10  # Remove the last digit
+                current_number += 1  # Increment the number
+
+        return lexicographical_numbers
         
