f9bb2a3f4ff15c9c9e309aaa5ad9ece7afc73b98
Sankalp Chavhan <55315838+SankalpC10@users.noreply.github.com>
2024-10-22 12:22:13 +0530

    Time: 59 ms (99.22%) | Memory: 52.9 MB (17.40%) - LeetSync

--- 2646-kth-largest-sum-in-a-binary-tree/kth-largest-sum-in-a-binary-tree.py
+++ 2646-kth-largest-sum-in-a-binary-tree/kth-largest-sum-in-a-binary-tree.py
@@ -9,19 +9,16 @@ class Solution:
         res = []
         q = deque([root])
         while q:
-            n = len(q)
             level_sum = 0
-            
-            for _ in range(n):
+            for _ in range(len(q)):
                 node = q.popleft()
                 level_sum += node.val
-
                 if node.left:
                     q.append(node.left)
                 if node.right:
                     q.append(node.right)
             res.append(level_sum)
-        if k> len(res):
+        if k>len(res):
             return -1
         res.sort(reverse = True)
         return res[k-1]
