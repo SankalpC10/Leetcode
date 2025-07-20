1c6dab05f7e401917727b37b26b6617cedbdd985
Sankalp Chavhan <55315838+SankalpC10@users.noreply.github.com>
2025-07-20 14:10:58 +0530

    Time: 98 ms (70.38%) | Memory: 28.8 MB (53.88%) - LeetSync

--- 1325-path-with-maximum-probability/path-with-maximum-probability.py
+++ 1325-path-with-maximum-probability/path-with-maximum-probability.py
@@ -1,22 +1,18 @@
 class Solution:
     def maxProbability(self, n: int, edges: List[List[int]], succProb: List[float], start_node: int, end_node: int) -> float:
-        max_prob = [0] * n
-        max_prob[start_node] = 1
-        
-        for i in range(n - 1):
-            # If there is no larger probability found during an entire round of updates,
-            # stop the update process.
-            has_update = 0
-            for j in range(len(edges)):
-                u, v = edges[j]
-                path_prob = succProb[j]
-                if max_prob[u] * path_prob > max_prob[v]:
-                    max_prob[v] = max_prob[u] * path_prob
-                    has_update = 1 
-                if max_prob[v] * path_prob > max_prob[u]:
-                    max_prob[u] = max_prob[v] * path_prob
-                    has_update = 1
-            if not has_update:
-                break
-        
-        return max_prob[end_node]
+        adj = collections.defaultdict(list)
+        for i in range(len(edges)):
+            src,dst = edges[i]
+            adj[src].append((dst,succProb[i]))
+            adj[dst].append((src,succProb[i]))
+        pq = [(-1,start_node)]
+        visit = set()
+        while pq:
+            prob,cur = heapq.heappop(pq)
+            visit.add(cur)
+            if cur==end_node:
+                return -prob
+            for nei,edgeProb in adj[cur]:
+                if nei not in visit:
+                    heapq.heappush(pq,(prob*edgeProb,nei))
+        return 0.0
